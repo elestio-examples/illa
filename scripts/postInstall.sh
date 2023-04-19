b06be68a3cf9679a7f5743f2c2bd359de7cd1b38
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-04-19 18:19:16 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -7,11 +7,8 @@ sleep 60s;
 
 target=$(docker-compose port illa-builder 80)
 
-curl http://${target}/supervisor/api/v1/auth/signup \
-  -H 'accept: application/json, text/plain, */*' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'content-encoding: gzip' \
-  -H 'content-type: application/json' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' \
-  --data-raw '{"language":"en-US","inviteToken":null,"isSubscribed":true,"nickname":"admin","email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'"}' \
-  --compressed
+docker exec -t illasds_illa-builder_1 sh -c "psql -U postgres postgres <<EOF
+\c illa_supervisor
+CREATE EXTENSION pgcrypto;
+UPDATE users SET password_digest=crypt('$ADMIN_PASSWORD', gen_salt('bf', 8)), email='$ADMIN_EMAIL' WHERE nickname = 'root';
+EOF";
