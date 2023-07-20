c7720f423597cf4be4d8e4fbdc726843c95a6112
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-07-20 21:55:44 +0530

    Update postInstall.sh for container name

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -7,7 +7,7 @@ sleep 60s;
 
 target=$(docker-compose port illa-builder 80)
 
-docker exec -t ${PIPELINE_NAME}_illa-builder_1 sh -c "psql -U postgres postgres <<EOF
+docker exec -t ${CONTAINER_NAME} sh -c "psql -U postgres postgres <<EOF
 \c illa_supervisor
 CREATE EXTENSION pgcrypto;
 UPDATE users SET password_digest=crypt('$ADMIN_PASSWORD', gen_salt('bf', 8)), email='$ADMIN_EMAIL' WHERE nickname = 'root';
